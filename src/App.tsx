a5332aa6d377c2b0448efc8d8edfec445d57d0b5
Olayinka Salaam <olayinkasalaam1001@gmail.com>
2025-09-27 19:30:35 +0100

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
             <Header />
             <main className="w-full">
               {/* Add an id for the hero section since your Header tries to scroll to it */}
-              <section id="hero" className="h-screen">
+              <section id="hero" className="min-h-screen">
                 <Hero />
               </section>
               <section id="about" className="h-screen">
